74643770de11e0f06d80e2c5a4df5323b6d6eedd
EddyJCuesta <cuesta79@gmail.com>
2023-05-05 22:12:08 -0400

    added a function that when the images are clicked, they appear in pop up window

--- script.js
+++ script.js
@@ -17,3 +17,15 @@ links.forEach(link => {
         navMenu.style.display = 'none';
     });
 });
+
+// Target the container element by its ID
+const container = document.getElementById('imageContainer');
+// get all the container's images and save to a variable
+const images = container.querySelectorAll('.images');
+// Loop through all images within the container and add a click function, when the image is clicked on a pop up window of the image will appear
+for(let i = 0; i < images.length; i++){
+    images[i].onclick = function(){
+        // new window will appear with the image
+        window.open(this.src, "Image", "width=400,height=400");
+    }
+}

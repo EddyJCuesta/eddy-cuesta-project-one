895a153ef6ce1e1ba7f1d95589df72f30c588791
EddyJCuesta <cuesta79@gmail.com>
2023-06-01 18:07:31 -0400

    add to cart function attempts

--- script.js
+++ script.js
@@ -2,10 +2,10 @@
 
 // Target the Hamburger (i) from font-awesome and save to a variable
 const hamburger = document.querySelector('#hamburger');
-console.log(hamburger);
+// console.log(hamburger);
 // Target the Navbar and save to a variable
 const navMenu = document.querySelector('.navBar');
-console.log(navMenu);
+// console.log(navMenu);
 // when the hamburger image is clicked and the NavBar is hidden,
 // The Nav bar containing (Home/Blog/Contact) will appear
 // when the Hamburger menu is clicked while the navBar is visable, the navbar will disapear 
@@ -36,34 +36,87 @@ for(let i = 0; i < images.length; i++){
     }
 }
 
+let list = [
+    {
+        image: './assets/p2.jpeg',
+        name: 'American Marigold',
+        price: '$23.45'
+    },
+    {
+        image: './assets/p1.jpeg',
+        name: 'Black Eyed Susan',
+        price: '$25.45'
+    },
+    {
+        image: './assets/p3.jpeg',
+        name: 'Bleeding Heart',
+        price: '$30.45'  
+    }
+]
+
+// const cartIcon = document.querySelectorAll('.cartIcon');
 // add item to cart
 // when you click on add to cart img the cart number will go up
-let cart = [];
-let count = 0;
+//     const addItemToCart = cartIcon.addEventListener('click', function(){
+    
+//     let nameItem = document.querySelector('.nameItem').value;
+//     let dollarColor = document.querySelector('.dollarColor').value;
 
-const counter = document.getElementById('.cartNum');
-const cartIcon = document.querySelectorAll('.cartIcon');
-const nameItem = document.querySelectorAll('.nameItem')[0].innerText; 
-const dollarColor = document.querySelectorAll('.dollarColor');
+//     let cart = [{
+//         name: nameItem,
+//         price: dollarColor,
+//     }];
 
-const itemContainer = document.querySelectorAll('.imageBox');
+//     for(let i = 0; i < list.length; i++){
+//         for(let j = 0; j < list[i].review.length; j++) {
+//             list[0].review[j].push(newData)
+//             console.log(list);
+//         }
+//     }
+//     console.log(addItemToCart);
+// })
 
 
-for(let i = 0; i < dollarColor.length; i++){
-   dollarColor[i].addEventListener('click', (event)=>{
-    const priceOfProduct = event.target.innerText;
-    cart.push(priceOfProduct);
-    console.log(cart);
-}); 
-}
+// let count = 0;
 
-for(let r = 0; r < nameItem.length; r++){
-    nameItem[r].addEventListener('click', (event)=> {
-        const nameOfProduct = event.target.innerText;
-        cart.push(nameOfProduct);
-        console.log(cart);
-    })
-}
+// const counter = document.getElementById('.cartNum');
+
+// const nameItem = document.querySelectorAll('.nameItem'); 
+// const dollarColor = document.querySelectorAll('.dollarColor');
+
+// const itemContainer = document.querySelectorAll('.imageBox');
+// let priceOfProduct;
+// let nameOfProduct;
+
+
+
+
+// for(let i = 0; i < dollarColor.length; i++){
+//    dollarColor[i].addEventListener('click', (event)=>{
+//     priceOfProduct = event.target.innerText;
+//     cart.push(priceOfProduct);
+//     console.log(cart);
+// }); 
+// }
+
+// console.log(nameItem);
+
+// for(let i = 0; i < nameItem.length; i++){
+//     nameItem[i].addEventListener('click', (event)=> {
+//         nameOfProduct = event.target.innerText;
+//         cart.push(nameOfProduct);
+//         console.log(cart);
+//     })
+// }
+
+// for(let i = 0; i < cartIcon.length; i++){
+//     cartIcon[i].addEventListener('click', (event)=> {
+//         const  addToCart = event.target.innerText;
+//         cart.push(nameOfProduct, priceOfProduct);
+//         console.log(cart);
+//     })
+// }
+
 
 // console.log(counter);
 // function addToCart(item, price){
